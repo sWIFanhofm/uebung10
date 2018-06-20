c68be8a1f8ebd8afbc5c05a20eab48718183f7a8
Andi Hofmann <andreas-h-online@web.de>
2018-06-20 10:11:20 +0200

    Aufgabe 1

--- src/main/java/ueb10/bremsen/Bremse.java
+++ src/main/java/ueb10/bremsen/Bremse.java
@@ -1,6 +1,23 @@
 package ueb10.bremsen;
 
-public interface Bremse {
-	void bremsen();
-	boolean brauchtService();
+public abstract class Bremse {
+
+	private String hersteller;
+	private String seriennummer;
+
+	public Bremse(String hersteller, String seriennummer){
+		this.hersteller = hersteller;
+		this.seriennummer = seriennummer;
+	}
+
+	public String getHersteller(){
+		return hersteller;
+	}
+
+	public String getSeriennummer(){
+		return seriennummer;
+	}
+
+	public abstract void bremsen();
+	public abstract boolean brauchtService();
 }
